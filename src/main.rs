7a58c3b0ee69c7823429f2b20517e87c290334d2
re1n-e <raghavendrabargali@gmail.com>
2025-10-04 18:40:35 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,11 +8,14 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream
+                    .write_all(b"+PONG\r\n")
+                    .expect("Failed to write to stream");
             }
             Err(e) => {
                 println!("error: {}", e);
