9ea9d4b3222570e03549ee5d0e9884744e20170d
re1n-e <raghavendrabargali@gmail.com>
2025-10-04 19:00:24 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,28 @@
-#![allow(unused_imports)]
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    // Uncomment this block to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    loop {
+        let stream = listener.accept().await;
 
-    for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 512];
-                loop {
-                    println!("accepted new connection");
-                    let read_count = stream
-                        .read(&mut buf)
-                        .expect("Failed to read the stream to buffer");
-                    if read_count == 0 {
-                        break;
+            Ok((mut stream, addr)) => {
+                println!("accepted new connection from: {addr}");
+
+                tokio::spawn(async move {
+                    let mut buf = [0; 512];
+                    loop {
+                        let read_count = stream.read(&mut buf).await.unwrap();
+                        if read_count == 0 {
+                            break;
+                        }
+
+                        stream.write(b"+PONG\r\n").await.unwrap();
                     }
-                    stream
-                        .write_all(b"+PONG\r\n")
-                        .expect("Failed to write back to the stream");
-                }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
